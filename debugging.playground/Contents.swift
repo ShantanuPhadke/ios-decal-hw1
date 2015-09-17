220ec8b780bcab482f49719cdc301c18f68d9f2a
ShantanuPhadke <shantanu.cricket@gmail.com>
2015-09-16 20:37:10 -0700

    Finished hmwk1

--- debugging.playground/Contents.swift
+++ debugging.playground/Contents.swift
@@ -15,39 +15,36 @@ class Foo {
     var wordB : String!
     
     init (words: [String?]) {
-        wordA = words[0]?
-        wordB = words[1]?
+        wordA = words[0]
+        wordB = words[1]
     }
-    
 //: [EXPLAIN YOUR ANSWER TO Q1 HERE]
-    
 
-    
 //: ## Q2: Variable Types and Function Types
 //: Why does the compiler dislike the for loop? Also, what should we return?
     
-    func arePalindromes(words: [String]) -> Bool! {
+    static func arePalindromes(words: [String]) -> Bool! {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
+        let numElements = words.count
         
-        for let i = 0; i < numElements; i++ {
+        for var i = 0; i < numElements; i++ {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
         
-        return nil
+        return true
     }
     
-//: [EXPLAIN YOUR ANSWER TO Q2 HERE]
+    //: The compiler dislikes the for loop because i was initially declared as a let, which cannot be changed. Instead it should have been declared as a var, which can change its value during the duration of the code. Also, we should return true, since if the string makes it past the loop
     
-    
+
     
 //: ## Q3: More functions, and object initialization
 //: The method should be returning true or false -- what's wrong?
 //: Are we initializing the dictionary correctly?
-    func isAnagram(wordA: String, wordB: String) -> Bool? {
-        var countLetters : [Character : Int]
+    static func isAnagram(wordA: String, wordB: String) -> Bool? {
+        var countLetters = [Character : Int]()
         var lenA = wordA.characters.count
         var lenB = wordB.characters.count
         
@@ -75,23 +72,23 @@ class Foo {
             }
         }
         
-        for (letter, count) in countLetters {
+        for count in countLetters.values{
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 }
 
-//: [EXPLAIN YOUR ANSWER TO Q3 HERE]
+//: [The proper way to initialize the dictionary is to change var countLetters: [Character : Int]().
 
 
 //: **Do not** change anything below.
 //: You should be able to call the methods as is.
-Foo.isAnagram("anagram", wordB: "managra")
-Foo.isAnagram("hello", wordB: "what")
+print(Foo.isAnagram("anagram", wordB: "managra"))
+print(Foo.isAnagram("hello", wordB: "what"))
 
 var palindromes = ["hih", "racecar", "mom", "wow"]
 var notPalindromes = ["gene", "shawn", "hello"]
